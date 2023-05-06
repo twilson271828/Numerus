12461f818a2ebc0fc9481ffc447ce2ac2559a942
Tyler James Wilson <grayghost@gmail.com>
2023-05-05 18:36:42 -0600

    Added discrete Fourier transform function + compiler change.

--- include/BigInt.hpp
+++ include/BigInt.hpp
@@ -22,7 +22,7 @@
             BigInt vmult(BigInt &x, BigInt &y) const;
             std::complex<double> exponentiate(size_t k,size_t n, size_t N);
             std::complex<double> dift(std::vector<std::complex<double>>& input,size_t n); 
-
+            std::complex<double> dft(std::vector<std::complex<double>>& input,size_t n); 
         public: 
 
         BigInt();
