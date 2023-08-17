ce93e0222eba05dfe4f21117fbcb9a6d39eb3db8
Tyler Wilson <grayghost@gmail.com>
2023-08-16 19:55:33 -0600

    Added karatsuba method to cryptome.cpp for debugging purposes.

--- src/cryptome.cpp
+++ src/cryptome.cpp
@@ -80,6 +80,48 @@ std::complex<double> dft(std::vector<std::complex<double>>& input,size_t n) {
     return coeff;
 } 
 
+BigInt karatsuba(BigInt &x, BigInt &y)  {
+
+    size_t n = x.size(); 
+    size_t m = y.size();
+
+    if (n < 10 && y < 10) {
+
+        return x*y;
+    }
+
+    size_t k = std::max(n,m);
+    size_t k2 = std::floor(k/2);
+
+    split split_x = x.split_it(k2);
+    split split_y = y.split_it(k2);
+    //BigInt z2 = split_x.x1*split_y.x1;
+    //BigInt z1 = split_x.x1*split_y.x0+split_x.x0*split_y.x1;
+    //BigInt z0 =split_x.x0*split_y.x0;
+    BigInt low_x = split_x.x0;
+    BigInt low_y = split_y.x0;
+    BigInt high_x = split_x.x1;
+    BigInt high_y = split_y.x1;
+    
+    BigInt xsum = low_x+high_x;
+    BigInt ysum = low_y+high_y;
+
+    BigInt z0 = karatsuba(low_x,low_y);
+    BigInt z1 = karatsuba(xsum,ysum);
+    BigInt z2 = karatsuba(high_x,high_y);
+
+    BigInt first = z2.m10(k2*2,false);
+    BigInt second = z1-z2-z0;
+    second = second.m10(k2,false);
+    BigInt third = z0;
+
+    return first + second + third;
+
+    
+    return x;
+}
+
+
 int main() {
 
   BigInt z("6789353555355553535353553538");
