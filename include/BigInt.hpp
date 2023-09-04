25b1f16dae8f0d56315c5cb6d31c7c73acf15891
grayghost <grayghost@core-dump>
2023-09-04 11:57:19 -0600

    clang formatted files.

--- include/BigInt.hpp
+++ include/BigInt.hpp
@@ -1,113 +1,102 @@
-    #include <iostream>
-    #include <cstdlib>
-    #include <vector>
-    #include <string>
-    #include <cmath>
-    #include <complex>
-    //#include <armadillo>
-    
-    enum SIGN {POS,NEG,UNDEFINED};
+#include <cmath>
+#include <complex>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+//#include <armadillo>
 
-    struct split;
+enum SIGN { POS, NEG, UNDEFINED };
 
-    class BigInt {
+struct split;
 
-        private:
+class BigInt {
 
-         std::vector<uint8_t> numerus;
+private:
+  std::vector<uint8_t> numerus;
 
-         SIGN sign;
+  SIGN sign;
 
-            BigInt vsub(BigInt &x,BigInt &y) const;
-            BigInt vadd(BigInt &x,BigInt &y) const;
-            BigInt vmult(BigInt &x, BigInt &y) const;
-            BigInt karatsuba(BigInt &x, BigInt &y) const;
-            BigInt Schonhage_Strassen(BigInt &x,BigInt&y) const;
-            BigInt Toom3(BigInt &x, BigInt &y) const;
-            std::complex<double> exponentiate(size_t k,size_t n,size_t N);
-            std::complex<double> dift(std::vector<std::complex<double>> &input,size_t n); 
-            std::complex<double> dft(std::vector<std::complex<double>> &input,size_t n); 
-            size_t bitrev(size_t n);
-        public: 
+  BigInt vsub(BigInt &x, BigInt &y) const;
+  BigInt vadd(BigInt &x, BigInt &y) const;
+  BigInt vmult(BigInt &x, BigInt &y) const;
+  BigInt karatsuba(BigInt &x, BigInt &y) const;
+  BigInt Schonhage_Strassen(BigInt &x, BigInt &y) const;
+  BigInt Toom3(BigInt &x, BigInt &y) const;
+  std::complex<double> exponentiate(size_t k, size_t n, size_t N);
+  std::complex<double> dift(std::vector<std::complex<double>> &input, size_t n);
+  std::complex<double> dft(std::vector<std::complex<double>> &input, size_t n);
+  size_t bitrev(size_t n);
 
-        BigInt();
-        
-        /// @brief 
-        /// @param c 
-        BigInt(const std::string c);
+public:
+  BigInt();
 
-        BigInt(const BigInt &num);
+  /// @brief
+  /// @param c
+  BigInt(const std::string c);
 
-        BigInt(const long &num);
+  BigInt(const BigInt &num);
 
-        /// @brief 
-        /// @param m 
-        /// @param add_to_front 
-        BigInt m10(const int m, bool add_to_front = false) const;
-        BigInt slice(int i,int j) const;
-        size_t operator[](const int i) const;
-        size_t size() const;
+  BigInt(const long &num);
 
-        int get_sign() const;
+  /// @brief
+  /// @param m
+  /// @param add_to_front
+  BigInt m10(const int m, bool add_to_front = false) const;
+  BigInt slice(int i, int j) const;
+  size_t operator[](const int i) const;
+  size_t size() const;
 
-        /// @brief 
-        void negative();
+  int get_sign() const;
 
-        
-        void insert(const int &val,const int &ix);
-                
-        BigInt operator *(const BigInt &num);
+  /// @brief
+  void negative();
 
-        /// @brief 
-        /// @param num 
-        /// @return 
-        
-        BigInt operator /(const BigInt &num) const;
+  void insert(const int &val, const int &ix);
 
-        BigInt operator+(const BigInt &num) const;
+  BigInt operator*(const BigInt &num);
 
-        BigInt operator -(const BigInt &num) const;
+  /// @brief
+  /// @param num
+  /// @return
 
-        bool operator < (const BigInt &num) const;
+  BigInt operator/(const BigInt &num) const;
 
-        bool operator <= (const BigInt &num) const;
+  BigInt operator+(const BigInt &num) const;
 
-        bool operator >= (const BigInt &num) const;
+  BigInt operator-(const BigInt &num) const;
 
-        bool operator > (const BigInt &num) const;
+  bool operator<(const BigInt &num) const;
 
-        bool operator != (const BigInt &num) const;
+  bool operator<=(const BigInt &num) const;
 
-        bool operator == (const BigInt &num) const;
+  bool operator>=(const BigInt &num) const;
 
-        BigInt operator ++ ();
+  bool operator>(const BigInt &num) const;
 
-        BigInt operator --();
+  bool operator!=(const BigInt &num) const;
 
-        BigInt operator ! ()const; 
+  bool operator==(const BigInt &num) const;
 
-        split split_it(size_t m) const;
+  BigInt operator++();
 
-        friend std::ostream& operator<<(std::ostream &out,const BigInt& num);
+  BigInt operator--();
 
-        
+  BigInt operator!() const;
 
-    };
+  split split_it(size_t m) const;
 
+  friend std::ostream &operator<<(std::ostream &out, const BigInt &num);
+};
 
-    struct split{
-        BigInt xleft;
-        BigInt xright;
-        size_t m;
-    };
+struct split {
+  BigInt xleft;
+  BigInt xright;
+  size_t m;
+};
 
-    inline void printSplit(const split& p){
-        std::cout << "high_p = " << p.xleft;
-        std::cout << "low_p = " << p.xright;
-        std::cout << "m = " << p.m << "\n";
-
-    }
-
-
-
-    
+inline void printSplit(const split &p) {
+  std::cout << "high_p = " << p.xleft;
+  std::cout << "low_p = " << p.xright;
+  std::cout << "m = " << p.m << "\n";
+}
--- src/cryptome.cpp
+++ src/cryptome.cpp
@@ -3,143 +3,124 @@
 #include <cmath>
 #include <limits>
 
-
 BigInt karatsuba(BigInt &x, BigInt &y) {
 
-    size_t n = x.size(); 
-    size_t m = y.size();
+  size_t n = x.size();
+  size_t m = y.size();
 
-    if (n > m) {
+  if (n > m) {
 
-        y = y.m10(n-m,true);
-    }
-    if (n < m) {
-        x = x.m10(m-n,true);
-    }
-        
+    y = y.m10(n - m, true);
+  }
+  if (n < m) {
+    x = x.m10(m - n, true);
+  }
 
-      if (n < 2 || m < 2) {
-        return x*y;
-    }
+  if (n < 2 || m < 2) {
+    return x * y;
+  }
 
-    size_t k = std::max(n,m);
-    size_t k2 = std::floor(k/2);
+  size_t k = std::max(n, m);
+  size_t k2 = std::floor(k / 2);
 
-    
-    split split_x = x.split_it(k2);    
-    split split_y = y.split_it(k2);
-    
- 
-    BigInt low_x = split_x.xright;
-    BigInt low_y = split_y.xright;
-    BigInt high_x = split_x.xleft;
-    BigInt high_y = split_y.xleft;
-    
-    BigInt z2 = karatsuba(high_x,high_y);
-    BigInt z0 = karatsuba(low_x,low_y);
-    BigInt w1 = high_x + low_x;
-    BigInt w2 = high_y + low_y;
+  split split_x = x.split_it(k2);
+  split split_y = y.split_it(k2);
 
-    BigInt z1 = karatsuba(w1,w2);
-    
-   BigInt W = z1-z2-z0;
-   
-   BigInt P = z2.m10(k2*2,false) + W.m10(k2,false)+ z0;
-   
-   return P;
-    
+  BigInt low_x = split_x.xright;
+  BigInt low_y = split_y.xright;
+  BigInt high_x = split_x.xleft;
+  BigInt high_y = split_y.xleft;
+
+  BigInt z2 = karatsuba(high_x, high_y);
+  BigInt z0 = karatsuba(low_x, low_y);
+  BigInt w1 = high_x + low_x;
+  BigInt w2 = high_y + low_y;
+
+  BigInt z1 = karatsuba(w1, w2);
+
+  BigInt W = z1 - z2 - z0;
+
+  BigInt P = z2.m10(k2 * 2, false) + W.m10(k2, false) + z0;
+
+  return P;
 }
-
-
 
 size_t bitrev(size_t n) {
 
-    //const size_t K = sizeof(n)*8;
-    const size_t K = 4;
-    std::cout << "K = " << K << "\n";
-    std::bitset<K> b(n);
+  // const size_t K = sizeof(n)*8;
+  const size_t K = 4;
+  std::cout << "K = " << K << "\n";
+  std::bitset<K> b(n);
 
-    std::cout << n << " in binary is " << b << "\n";
+  std::cout << n << " in binary is " << b << "\n";
 
-    return 0;
+  return 0;
 }
 
-std::complex<double> exponentiate(size_t k,size_t n,size_t N) {
+std::complex<double> exponentiate(size_t k, size_t n, size_t N) {
 
-    double x = 0.0;
-    double y = 0.0;
-    double theta = ((2* M_PI *k * n)/N);
-   
-    x = std::cos(theta);
-    y = std::sin(theta);
-    std::complex<double> omega(x,y);
-    return omega;    
+  double x = 0.0;
+  double y = 0.0;
+  double theta = ((2 * M_PI * k * n) / N);
+
+  x = std::cos(theta);
+  y = std::sin(theta);
+  std::complex<double> omega(x, y);
+  return omega;
 }
 
+BigInt Schonhage_Strassen(BigInt &x, BigInt &y) { return x; }
 
-BigInt Schonhage_Strassen(BigInt &x,BigInt&y) {
+std::complex<double> dift(std::vector<std::complex<double>> &input, size_t n) {
 
-    return x;
+  size_t N = input.size();
+  std::complex<double> coeff(0.0, 0.0);
+
+  for (int k = 0; k < N; k++) {
+    coeff += input[k] * exponentiate(k, n, N);
+  }
+
+  if (abs(coeff.real()) < 1e-6) {
+    coeff.real(0.0);
+  }
+
+  if (abs(coeff.imag()) < 1e-6) {
+    coeff.imag(0.0);
+  }
+
+  coeff /= N;
+  return coeff;
 }
 
+std::complex<double> dft(std::vector<std::complex<double>> &input, size_t n) {
 
+  size_t N = input.size();
+  std::complex<double> coeff(0.0, 0.0);
 
-std::complex<double> dift(std::vector<std::complex<double>>& input, size_t n) {
+  for (int k = 0; k < N; k++) {
+    coeff += input[k] / exponentiate(k, n, N);
+  }
 
-    size_t N = input.size();
-    std::complex<double> coeff(0.0,0.0);
-    
-    for (int k = 0; k < N; k++) {   
-            coeff += input[k]*exponentiate(k,n,N);       
-    }
-    
+  if (abs(coeff.real()) < 1e-6) {
+    coeff.real(0.0);
+  }
 
-    if (abs(coeff.real()) < 1e-6) {
-        coeff.real(0.0);
-    }
+  if (abs(coeff.imag()) < 1e-6) {
+    coeff.imag(0.0);
+  }
 
-    if (abs(coeff.imag()) < 1e-6) {
-       coeff.imag(0.0);    
-    }
-
-
-    coeff /= N;
-    return coeff;
-} 
-
-std::complex<double> dft(std::vector<std::complex<double>>& input,size_t n) {
-
-    size_t N = input.size();
-    std::complex<double> coeff(0.0,0.0);
-    
-    for (int k = 0; k < N; k++) {   
-            coeff += input[k]/exponentiate(k,n,N);       
-    }
-
-    if (abs(coeff.real()) < 1e-6) {
-        coeff.real(0.0);
-    }
-
-    if (abs(coeff.imag()) < 1e-6) {
-        coeff.imag(0.0);    
-    }
-
-    return coeff;
-} 
-
+  return coeff;
+}
 
 int main() {
 
   BigInt z1("12345");
   BigInt z2("6789");
 
-  
-  BigInt z5 = karatsuba(z1,z2);
-  std::cout << "z5 = " << z5 <<"\n";
+  BigInt z5 = karatsuba(z1, z2);
+  std::cout << "z5 = " << z5 << "\n";
 
-  BigInt z6 = z1*z2;
+  BigInt z6 = z1 * z2;
 
   std::cout << "z6 = " << z6 << "\n";
-
-
 }
