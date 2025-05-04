add722d6678a800552e2ad04ea7140591b7704b0
Tyler Wilson <grayghost@gmail.com>
2025-05-03 20:33:42 -0600

    Added fftcrypt to play around with different fft algorithms in python.

---
+++ src/python/fftcrypt.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+
+if __name__=="__main__":
+    # Example usage
+    signal = np.random.randint(0,9,16)  # Example signal
+    print(signal);
